1f1e77d8b5e6ad716623b859d2d6e7eb41aeedae
bhupender singh <bhupender2403@gmail.com>
2026-07-14 18:06:20 +0530

    Add funtionality for calculating similarity scores between two treediffs -Add test for the same

--- src/bsk_git_viewer/inference/compute_commit_similarity.py
+++ src/bsk_git_viewer/inference/compute_commit_similarity.py
@@ -1,6 +1,9 @@
 
 
-from bsk_git_viewer.models import  CommitWithPreDiff
+from dataclasses import dataclass
+
+from bsk_git_viewer.models import  CommitWithPreDiff, FileChange, TreeDiff
+from bsk_git_viewer.utils.sequence_matching import match_sequences
 
 
 def extract_candidates_for_similarity(commit:CommitWithPreDiff, commit_list:list[CommitWithPreDiff]):
@@ -24,4 +27,79 @@ def extract_candidates_for_similarity(commit:CommitWithPreDiff, commit_list:list
                 candidates.append(candidate)
 
         return candidates
+    
+
+def compute_similarity_score(diff_one:TreeDiff, diff_two:TreeDiff):
+    paths = {x.path:x for x in diff_one.files}
+
+    other_paths = {x.path:x for x in diff_two.files}
+    
+    similar_paths = paths.intersection(other_paths)
+
+    if len(similar_paths)==0:
+        return 0
+    
+    total_score = 0
+
+    for path in similar_paths:
+        change1 =  paths[path]
+        change2 = other_paths[path]
+        
+        total_score+= compute_similarity_score_for_change(change1, change2)
+        
+    return total_score/len(similar_paths) if len(similar_paths)>0 else 0
+
+
+
+def compute_similarity_score_for_change(change:FileChange, other_change:FileChange):
+    if change.path!=other_change.path:
+        return 0
+    else:
+        if change.status!=other_change.status:
+            if change.status=="deleted" or other_change.status=="deleted":
+                return 0
+            else:
+                return 20
+        else:
+            if change.status!="modified":
+                return 100
+            else:
+                additions1 = [x.content for x in change.additions]
+                additions2 = [x.content for x in other_change.additions]
+
+                deletions1 = [x.content for x in change.deletions]
+                deletions2 = [x.content for x in other_change.deletions]
+
+                add_score = compute_changeset_score(additions1, additions2)
+                del_score = compute_changeset_score(deletions1, deletions2)
+            
+                matching_score = 0
+                if add_score.total==0:
+                    matching_score = del_score.score
+                elif del_score.total==0:
+                    matching_score = add_score.score
+                else:
+                    matching_score = (add_score.score+del_score.score)/2
+                
+                return 20+ matching_score*80/100
+    
+@dataclass
+class SimilarityScore:
+    score:float
+    match_count:int
+    total:int
+
+def compute_changeset_score(sequence1:list[str], sequence2:list[str]) -> SimilarityScore:
+    additions, deletions = match_sequences(sequence1, sequence2)
+
+    not_matched = len(additions)+len(deletions)
+    total = len(sequence1)+len(sequence2)
+    matched =  total - not_matched
+
+    # Matches are counted twice
+    unique_match_count = int(matched/2)
+
+    final_score = matched/total*100 if total>0 else 25
+
+    return SimilarityScore(final_score, unique_match_count, total)
     
--- tests/unit/inference/test_comupute_commit_similarity.py
+++ tests/unit/inference/test_comupute_commit_similarity.py
@@ -2,9 +2,11 @@
 from dataclasses import dataclass
 from unittest.mock import MagicMock
 
+import pytest
 
-from bsk_git_viewer.inference.compute_commit_similarity import extract_candidates_for_similarity
-from bsk_git_viewer.models import CommitInfo, CommitWithPreDiff, FileChange, TreeDiff
+
+from bsk_git_viewer.inference.compute_commit_similarity import compute_changeset_score, compute_similarity_score_for_change, extract_candidates_for_similarity
+from bsk_git_viewer.models import CommitInfo, CommitWithPreDiff, FileChange, LineChange, TreeDiff
 
 
 def test_extract_candidates_for_similarity():
@@ -90,3 +92,91 @@ def test_extract_candidates_for_similarity_no_match():
 
     assert len(result)==0
 
+
+@pytest.mark.parametrize(
+        "sequences1,sequences2,score_exp,match,total",
+        [
+            ([],[],25, 0, 0),
+            (["a1"],[],0 , 0, 1),
+            (["a1","a2"],["a2"],66.67, 1, 3),
+            (["a1","a2"],["a1","a2"],100, 2, 4),
+            (["a1","a2","a3"],["a1","a4","a3"],66.66, 2, 6),
+        ]
+)
+def test_compute_changeset_score(sequences1, sequences2, score_exp, match, total):
+    score = compute_changeset_score(sequences1, sequences2)
+
+    assert score.score == pytest.approx(score_exp,abs=1e-2)
+    assert score.match_count == match
+    assert score.total == total
+
+
+
+@pytest.mark.parametrize(
+        "addition1, deletion1, addition2, deletion2, score_exp",
+        [
+           ([],[],[],[],40),
+           (["a1"],[],[],[],20),
+           (["a1"],[],[],["a2"],20),
+           (["a1"],[],["a1"],["a2"],60),
+           (["a1"],["a2"],["a1"],["a2"],100),
+           (["a1","a4","a5"],["a2"],["a1","a4","a5","a6"],["a2"],94.29),
+           (["a1","a4","a5"],["a2"],["a11","a41","a5","a61"],["a21"],31.42),
+
+        ]
+)
+def test_compute_similarity_score_for_change_file_modification(addition1, deletion1, addition2, deletion2, score_exp):
+    
+
+    def mock_change(pathV,status,additions, deletions):
+        change1 = MagicMock(spec = FileChange)
+        change1.path = pathV
+        change1.status = status
+        add_list = []
+        for x in additions:
+            add_v = MagicMock(spec = LineChange)
+            add_v.content = x
+            add_v.type = "add"
+            add_list.append(add_v)
+        
+        del_list = []
+        for x in deletions:
+            del_v = MagicMock(spec = LineChange)
+            del_v.content = x
+            del_v.type = "delete"
+            del_list.append(del_v)
+        
+        change1.additions = add_list
+        change1.deletions = del_list
+        return change1
+
+
+    change1 = mock_change("f1","modified",addition1, deletion1)
+    change2 = mock_change("f1","modified",addition2, deletion2)
+    
+    score = compute_similarity_score_for_change(change1, change2)
+    assert score == pytest.approx(score_exp,0.01)
+
+
+@pytest.mark.parametrize(
+        "status1, status2, score_exp",
+        [
+            ("added","added",100),
+            ("deleted","deleted",100),
+            ("added","deleted", 0),
+            ("deleted","added", 0),
+            ("deleted","modified", 0),
+            ("modified","added", 20),
+        ]
+)
+def test_compute_similarity_score_for_change_status(status1, status2, score_exp):
+    change1 = MagicMock(spec = FileChange)
+    change1.path = "f1"
+    change1.status = status1
+
+    change2 = MagicMock(spec = FileChange)
+    change2.path = "f1"
+    change2.status = status2
+
+    score = compute_similarity_score_for_change(change1, change2)
+    assert score == score_exp
