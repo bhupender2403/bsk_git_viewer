c1b47f4f8a1dd78ea7fd783ce274667ff04cb431
bhupender singh <bhupender2403@gmail.com>
2026-07-15 18:15:41 +0530

    Return all the commits from the git store.

--- src/bsk_git_viewer/git/commit_sequence_extraction.py
+++ src/bsk_git_viewer/git/commit_sequence_extraction.py
@@ -1,14 +1,29 @@
+from dulwich.objects import Commit
+
 from bsk_git_viewer.models import CommitInfo
 from bsk_git_viewer.utils.parse_utils import to_commit_info
 
 
 def extract_other_commits(repo, main_commits:list[CommitInfo]) -> list[CommitInfo]:
+    all_commits = extract_all_commits(repo)
+
     main_commit_set = {x.commit_id for x in main_commits}
 
     return [
-        x for x in repo.get_walker() 
+        x for x in all_commits
         if x.commit_id not in main_commit_set
     ]
+
+def extract_all_commits(repo):
+
+    all_commits = {}
+    for sha in repo.object_store:
+        obj = repo.object_store[sha]
+
+        if isinstance(obj, Commit):
+            all_commits.append(to_commit_info(obj)) 
+
+    return all_commits
 
 # ===============================================================================================
 # Main commits: ref/heads/main
