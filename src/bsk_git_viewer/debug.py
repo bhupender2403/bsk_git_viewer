b4fe23d1f71676ae241e3cb8981b3adc86f30682
bhupender singh <bhupender2403@gmail.com>
2026-07-16 18:13:25 +0530

    Add debug script to run git commands from command line

--- src/bsk_git_viewer/debug.py
+++ src/bsk_git_viewer/debug.py
@@ -1,12 +1,34 @@
+import os
+from pathlib import Path
+
 import typer
 
 from bsk_git_viewer.git_reader import analyze_repository, read_repository
+from bsk_git_viewer.gitcreate.git_sceneria_creator import GitScenarioRunner, parse_scenario
 
 app = typer.Typer()
 
 @app.command()
-def repo(repo_path:str="."):
-    analyze_repository(repo_path)
+def gitrun(
+    repo_path:str,
+    command_file:str
+):
+    repo_path = Path(repo_path)
+
+    if os.path.exists(repo_path):
+        typer.echo("Path already exist so it cannot be run again")
+    
+    os.mkdir(repo_path)
+
+    with open(command_file,"rt") as fd:
+        command_text = fd.read()
+    
+    commands = parse_scenario(
+        command_text
+    )
+    gitrunner = GitScenarioRunner(repo_path)
+
+    gitrunner.run(commands)
     
 if __name__=="__main__":
     app()
